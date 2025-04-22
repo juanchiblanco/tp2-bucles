d84f88893db0c495c17ee78ebdff58b9e72d0c1f
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-22 12:44:24 -0300

    ejercicio8 completado

--- js/ejercicio8.js
+++ js/ejercicio8.js
@@ -1,12 +1,11 @@
 const numero = parseInt(prompt(`Ingrese un numero entre 1 y 50`))
 if(numero<1 || numero>50 || isNaN(numero)){alert(`numero invalido`)}
 
-if(numero>=1){
+if(numero>=1 && numero<=50){
     for(let i=1; i<=numero;i++)
         {
-            for(let caracteres=0;caracteres<i;caracteres++)
-            document.writeln(i)
+            for(let caracteres=1;caracteres<=i;caracteres++)
+            document.writeln(caracteres)
             document.writeln(`<br>`)
         }
 }
-

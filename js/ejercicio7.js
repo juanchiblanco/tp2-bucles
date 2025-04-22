d9c2ade8aee7e04d49840d73f21e779fc6bbb8c9
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-22 12:01:42 -0300

    primera parte del ejercicio completa

---
+++ js/ejercicio7.js
@@ -0,0 +1,14 @@
+// for(let i=1; i<=30;i++)
+//     {
+//         for(let caracteres=0;caracteres<i;caracteres++)
+//         document.writeln(i)
+//         document.writeln(`<br>`)   
+//     }
+
+const numero = parseInt(prompt(`Ingrese un numero entre 1 y 50`))
+if(numero<1 || numero>50 || isNaN(numero)){alert(`numero invalido`)}
+
+if(numero>=1){
+    for(let i=numero; i>=1;i--){
+    }
+}

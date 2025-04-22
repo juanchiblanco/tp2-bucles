90d1bb8f5e5aa14787f7ab77e350f9d3cfe8e5d8
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-22 13:34:25 -0300

    ejercicio 11 completo

---
+++ js/ejercicio11.js
@@ -0,0 +1,14 @@
+const persona1 = prompt(`Ingrese el nombre de la primera persona`)
+const edad1 = parseInt(prompt(`Ingrese la edad de ${persona1}`))
+const persona2 = prompt(`Ingrese el nombre de la segunda persona`)
+const edad2 = parseInt(prompt(`Ingrese la edad de ${persona2}`))
+const persona3 = prompt(`Ingrese el nombre de la tercera persona`)
+const edad3 = parseInt(prompt(`Ingrese la edad de ${persona3}`))
+if(isNaN(edad3, edad2. edad1)){alert(`Ingresaste numeros invalidos`)}
+
+let mayor=Math.max(edad1,edad2,edad3)
+console.log(mayor)
+
+if(mayor=edad1){document.writeln(`La persona mayor es ${persona1} con ${edad1} años`)}
+else if(mayor=edad2){document.writeln(`La persona mayor es ${persona2} con ${edad2} años`)}
+else if(mayor=edad3){document.writeln(`La persona mayor es ${persona3} con ${edad3} años`)}

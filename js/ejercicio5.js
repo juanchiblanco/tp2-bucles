5ac8ffbdb036def2c34d3914d19c0417e145e055
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-22 11:33:31 -0300

    termine ejercicio 5

---
+++ js/ejercicio5.js
@@ -0,0 +1,7 @@
+const dni = parseInt(prompt(`Ingrese su numero de DNI sin puntos (8 caracteres max.)`))
+if(dni>99999999 || dni<0 || isNaN(dni)){alert(`Numero invalido`)}
+const resto = dni % 23
+const letras = "TRWAGMYFPDXBNJZSQVHLCKE"
+const letra = letras.charAt(resto);
+
+document.writeln(`DNI: ${dni} - Letra: ${letra}`);

4090762a2a8dfda37a7bb8418c72364c1bc68191
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-22 12:05:28 -0300

    ejercicio 7 completado el bucle con la piramide invertidad

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -10,5 +10,8 @@ if(numero<1 || numero>50 || isNaN(numero)){alert(`numero invalido`)}
 
 if(numero>=1){
     for(let i=numero; i>=1;i--){
+        for(let caracteres=0;caracteres<i;caracteres++)
+                    document.writeln(i)
+                    document.writeln(`<br>`) 
     }
 }
